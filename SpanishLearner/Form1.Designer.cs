91eb540a0e0bf89bc07d94614d03151af26ff501
torben <tobi_as1995@hotmail.de>
2019-08-27 15:52:03 +0200

    vocabulary trainer can be now used with some data in the database

---
+++ SpanishLearner/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace SpanishLearner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.allgemeinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testBeendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.programmBeendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuDirection = new System.Windows.Forms.ToolStripMenuItem();
+            this.deutschSpanischToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.spanischDeutschToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deutschPortugiesischToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.portugiesischDeutschToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hilfeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.anleitungToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.labelVokabel = new System.Windows.Forms.Label();
+            this.textBoxEingabe = new System.Windows.Forms.TextBox();
+            this.labelAnzeige = new System.Windows.Forms.Label();
+            this.deutschEnglishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.englishDeutschToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.allgemeinToolStripMenuItem,
+            this.MenuDirection,
+            this.hilfeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // allgemeinToolStripMenuItem
+            // 
+            this.allgemeinToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testBeendenToolStripMenuItem,
+            this.programmBeendenToolStripMenuItem});
+            this.allgemeinToolStripMenuItem.Name = "allgemeinToolStripMenuItem";
+            this.allgemeinToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.allgemeinToolStripMenuItem.Text = "Allgemein";
+            // 
+            // testBeendenToolStripMenuItem
+            // 
+            this.testBeendenToolStripMenuItem.Name = "testBeendenToolStripMenuItem";
+            this.testBeendenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.testBeendenToolStripMenuItem.Text = "Test beenden";
+            this.testBeendenToolStripMenuItem.Click += new System.EventHandler(this.testBeendenToolStripMenuItem_Click);
+            // 
+            // programmBeendenToolStripMenuItem
+            // 
+            this.programmBeendenToolStripMenuItem.Name = "programmBeendenToolStripMenuItem";
+            this.programmBeendenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.programmBeendenToolStripMenuItem.Text = "Programm beenden";
+            this.programmBeendenToolStripMenuItem.Click += new System.EventHandler(this.programmBeendenToolStripMenuItem_Click);
+            // 
+            // MenuDirection
+            // 
+            this.MenuDirection.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deutschSpanischToolStripMenuItem,
+            this.spanischDeutschToolStripMenuItem,
+            this.deutschPortugiesischToolStripMenuItem,
+            this.portugiesischDeutschToolStripMenuItem,
+            this.deutschEnglishToolStripMenuItem,
+            this.englishDeutschToolStripMenuItem});
+            this.MenuDirection.Name = "MenuDirection";
+            this.MenuDirection.Size = new System.Drawing.Size(67, 20);
+            this.MenuDirection.Text = "Richtung";
+            // 
+            // deutschSpanischToolStripMenuItem
+            // 
+            this.deutschSpanischToolStripMenuItem.Name = "deutschSpanischToolStripMenuItem";
+            this.deutschSpanischToolStripMenuItem.Size = new System.Drawing.Size(261, 22);
+            this.deutschSpanischToolStripMenuItem.Text = "Deutsch - Spanisch";
+            this.deutschSpanischToolStripMenuItem.Click += new System.EventHandler(this.deutschSpanischToolStripMenuItem_Click);
+            // 
+            // spanischDeutschToolStripMenuItem
+            // 
+            this.spanischDeutschToolStripMenuItem.Name = "spanischDeutschToolStripMenuItem";
+            this.spanischDeutschToolStripMenuItem.Size = new System.Drawing.Size(261, 22);
+            this.spanischDeutschToolStripMenuItem.Text = "Spanisch - Deutsch (Voreinstellung)";
+            this.spanischDeutschToolStripMenuItem.Click += new System.EventHandler(this.spanischDeutschToolStripMenuItem_Click);
+            // 
+            // deutschPortugiesischToolStripMenuItem
+            // 
+            this.deutschPortugiesischToolStripMenuItem.Name = "deutschPortugiesischToolStripMenuItem";
+            this.deutschPortugiesischToolStripMenuItem.Size = new System.Drawing.Size(261, 22);
+            this.deutschPortugiesischToolStripMenuItem.Text = "Deutsch - Portugiesisch";
+            this.deutschPortugiesischToolStripMenuItem.Click += new System.EventHandler(this.deutschPortugiesischToolStripMenuItem_Click_1);
+            // 
+            // portugiesischDeutschToolStripMenuItem
+            // 
+            this.portugiesischDeutschToolStripMenuItem.Name = "portugiesischDeutschToolStripMenuItem";
+            this.portugiesischDeutschToolStripMenuItem.Size = new System.Drawing.Size(261, 22);
+            this.portugiesischDeutschToolStripMenuItem.Text = "Portugiesisch - Deutsch";
+            this.portugiesischDeutschToolStripMenuItem.Click += new System.EventHandler(this.portugiesischDeutschToolStripMenuItem_Click_1);
+            // 
+            // hilfeToolStripMenuItem
+            // 
+            this.hilfeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.anleitungToolStripMenuItem});
+            this.hilfeToolStripMenuItem.Name = "hilfeToolStripMenuItem";
+            this.hilfeToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.hilfeToolStripMenuItem.Text = "Hilfe";
+            // 
+            // anleitungToolStripMenuItem
+            // 
+            this.anleitungToolStripMenuItem.Name = "anleitungToolStripMenuItem";
+            this.anleitungToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.anleitungToolStripMenuItem.Text = "Anleitung";
+            this.anleitungToolStripMenuItem.Click += new System.EventHandler(this.anleitungToolStripMenuItem_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(31, 330);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(104, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Test starten";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Enabled = false;
+            this.buttonNext.Location = new System.Drawing.Point(31, 368);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(104, 23);
+            this.buttonNext.TabIndex = 2;
+            this.buttonNext.Text = "Prüfen / Nächster";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // labelVokabel
+            // 
+            this.labelVokabel.AutoSize = true;
+            this.labelVokabel.Location = new System.Drawing.Point(182, 335);
+            this.labelVokabel.Name = "labelVokabel";
+            this.labelVokabel.Size = new System.Drawing.Size(0, 13);
+            this.labelVokabel.TabIndex = 3;
+            // 
+            // textBoxEingabe
+            // 
+            this.textBoxEingabe.Enabled = false;
+            this.textBoxEingabe.Location = new System.Drawing.Point(171, 370);
+            this.textBoxEingabe.Name = "textBoxEingabe";
+            this.textBoxEingabe.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEingabe.TabIndex = 4;
+            // 
+            // labelAnzeige
+            // 
+            this.labelAnzeige.AutoSize = true;
+            this.labelAnzeige.Location = new System.Drawing.Point(28, 296);
+            this.labelAnzeige.Name = "labelAnzeige";
+            this.labelAnzeige.Size = new System.Drawing.Size(101, 13);
+            this.labelAnzeige.TabIndex = 5;
+            this.labelAnzeige.Text = "spanisch / deutsch ";
+            // 
+            // deutschEnglishToolStripMenuItem
+            // 
+            this.deutschEnglishToolStripMenuItem.Name = "deutschEnglishToolStripMenuItem";
+            this.deutschEnglishToolStripMenuItem.Size = new System.Drawing.Size(261, 22);
+            this.deutschEnglishToolStripMenuItem.Text = "Deutsch - English";
+            this.deutschEnglishToolStripMenuItem.Click += new System.EventHandler(this.deutschEnglishToolStripMenuItem_Click);
+            // 
+            // englishDeutschToolStripMenuItem
+            // 
+            this.englishDeutschToolStripMenuItem.Name = "englishDeutschToolStripMenuItem";
+            this.englishDeutschToolStripMenuItem.Size = new System.Drawing.Size(261, 22);
+            this.englishDeutschToolStripMenuItem.Text = "English - Deutsch";
+            this.englishDeutschToolStripMenuItem.Click += new System.EventHandler(this.englishDeutschToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelAnzeige);
+            this.Controls.Add(this.textBoxEingabe);
+            this.Controls.Add(this.labelVokabel);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "VokabelTrainer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem allgemeinToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem testBeendenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem programmBeendenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MenuDirection;
+        private System.Windows.Forms.ToolStripMenuItem deutschSpanischToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem spanischDeutschToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deutschPortugiesischToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem portugiesischDeutschToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hilfeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem anleitungToolStripMenuItem;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Label labelVokabel;
+        private System.Windows.Forms.TextBox textBoxEingabe;
+        private System.Windows.Forms.Label labelAnzeige;
+        private System.Windows.Forms.ToolStripMenuItem deutschEnglishToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem englishDeutschToolStripMenuItem;
+    }
+}
+
---
+++ SpanishLearner/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+namespace SpanishLearner
+{
+    public partial class Form1 : Form
+    {
+        private List<string> quest = new List<string>();
+
+        private List<string> res = new List<string>();
+
+        private int random;
+
+        private int direction;
+
+        private Random r = new Random();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            direction = 2; // Spanisch - Deutsch = 2
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection();
+
+            OleDbCommand comm = new OleDbCommand();
+
+            OleDbDataReader reader;
+
+            conn.ConnectionString =
+                "Provider=Microsoft.ACE.OLEDB.12.0;" +
+                "Data Source=E:\\Informatik\\Software\\C#\\Windows Forms\\SpanishLearner\\SpanishLearner\\vocabulary.accdb";
+            
+            comm.Connection = conn;
+
+            comm.CommandText = "SELECT * FROM vocabel";
+
+            quest.Clear();
+            
+            res.Clear();
+
+            try
+            {
+                conn.Open();
+                reader = comm.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    // question
+                    if (direction == 1 || direction == 3 || direction == 5)
+                        quest.Add((string)reader["deutsch"]);
+
+                    else if (direction == 2)
+                        quest.Add((string)reader["Spanisch"]);
+
+                    else if (direction == 4)
+                        quest.Add((string)reader["portugiesisch"]);
+
+                    else
+                        quest.Add((string)reader["english"]);
+
+                    // response
+                    if (direction == 2 || direction == 4 || direction == 6)
+                        res.Add((string)reader["deutsch"]);
+
+                    else if (direction == 1)
+                        res.Add((string)reader["spanisch"]);
+
+                    else if (direction == 3)
+                        res.Add((string)reader["portugiesisch"]);
+
+                    else
+                        res.Add((string)reader["english"]);
+                }
+
+                reader.Close();
+
+                conn.Close();
+
+                buttonStart.Enabled = false;
+
+                buttonNext.Enabled = true;
+
+                MenuDirection.Enabled = false;
+
+                textBoxEingabe.Enabled = true;
+
+                ShowNext();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ShowNext()
+        {
+            if (quest.Count < 1) {
+                MessageBox.Show("Gratuliere! Alles geschafft");
+                Restart();
+            }
+            else
+            {
+                random = r.Next(0, quest.Count);
+
+                labelVokabel.Text = "" + quest[random];
+
+                textBoxEingabe.Text = "";
+            }
+        }
+
+        private void Restart()
+        {
+            buttonStart.Enabled = true;
+
+            buttonNext.Enabled = false;
+
+            MenuDirection.Enabled = true;
+
+            textBoxEingabe.Enabled = false;
+
+
+
+
+            labelVokabel.Text = "";
+
+            textBoxEingabe.Text = "";
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (textBoxEingabe.Text == (string)res[random])
+            {
+                MessageBox.Show("Richtig", "Vokabel");
+
+                quest.RemoveAt(random);
+
+                res.RemoveAt(random);
+            }
+            else
+                MessageBox.Show("Falsch, richtige Antwort" +
+                    " ist\n'" + res[random] +
+                    "'", "Vokabel");
+
+
+
+            ShowNext();
+            
+        }
+
+        private void testBeendenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Test wirklich beenden?", "Vokabel",
+                    MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Restart();
+        }
+
+        private void programmBeendenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Programm wirklich beenden?", "Vokabel",
+                    MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Close();
+        }
+
+        private void deutschSpanischToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            direction = 1;
+
+            Ignore();
+
+            deutschSpanischToolStripMenuItem.Checked = true;
+
+            labelAnzeige.Text = "deutsch/spanisch";
+        }
+
+        private void spanischDeutschToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            direction = 2;
+
+            Ignore();
+
+            spanischDeutschToolStripMenuItem.Checked = true;
+
+            labelAnzeige.Text = "spanisch/deutsch";
+        }
+
+        private void deutschPortugiesischToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            direction = 3;
+
+            Ignore();
+
+            deutschPortugiesischToolStripMenuItem.Checked = true;
+
+            labelAnzeige.Text = "deutsch/portugiesisch";
+        }
+
+        private void portugiesischDeutschToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            direction = 4;
+
+            Ignore();
+
+            portugiesischDeutschToolStripMenuItem.Checked = true;
+
+            labelAnzeige.Text = "portugiesisch/deutsch";
+        }
+
+        private void deutschEnglishToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            direction = 5;
+
+            Ignore();
+
+            deutschEnglishToolStripMenuItem.Checked = true;
+
+            labelAnzeige.Text = "deutsch/english";
+        }
+
+        private void englishDeutschToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            direction = 6;
+
+            Ignore();
+
+            englishDeutschToolStripMenuItem.Checked = true;
+
+            labelAnzeige.Text = "english/deutsch";
+        }
+
+        private void Ignore()
+        {
+            deutschSpanischToolStripMenuItem.Checked = false;
+            spanischDeutschToolStripMenuItem.Checked = false;
+            deutschPortugiesischToolStripMenuItem.Checked = false;
+            portugiesischDeutschToolStripMenuItem.Checked = false;
+            deutschEnglishToolStripMenuItem.Checked = false;
+            englishDeutschToolStripMenuItem.Checked = false;
+        }
+
+        private void anleitungToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileStream fs;
+
+            StreamReader sr;
+
+            string file = "hilfe.txt";
+
+            string output;
+
+            if (!File.Exists(file))
+            {
+                MessageBox.Show("Die Datei " + file +
+                    " existiert nicht");
+                return;
+            }
+
+            fs = new FileStream(file, FileMode.Open);
+
+            sr = new StreamReader(fs);
+
+            output = "";
+
+            while (sr.Peek() != -1)
+            {
+                output += sr.ReadLine() + "\n";
+            }
+
+            sr.Close();
+
+
+
+            MessageBox.Show(output);
+
+
+        }
+
+
+    }
+}
